1ac7abc89703547e2f7a885ff6e8db69eec71b95
Abdelmajed-zeyad <abd.d@live.com>
2021-09-07 00:29:26 +0300

    more  files

--- drf_api_permissions_postgres/urls.py
+++ drf_api_permissions_postgres/urls.py
@@ -18,6 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("api/v1/premissions/", include("posts.urls")),
+    path("api/v1/premissions/", include("premissions.urls")),
     path("api-auth/", include("rest_framework.urls")),
 ]
